8c030dd22e6971d721120867e1e4dba935c812c3
shihedelong <shihedelong@gmail.com>
2020-09-12 19:47:29 +1000

    Updates

---
+++ precache-manifest.4207c8501e75c942feba9edd7829e919.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "194c6bccf6ff2a99158b7ed0dc3c3d02",
+    "url": "/cssa/index.html"
+  },
+  {
+    "revision": "f09f10db591ed3eb4f77",
+    "url": "/cssa/static/css/main.1d632b7c.chunk.css"
+  },
+  {
+    "revision": "f52d04af3d97578663e0",
+    "url": "/cssa/static/js/2.990cd933.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cssa/static/js/2.990cd933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f09f10db591ed3eb4f77",
+    "url": "/cssa/static/js/main.1812c2f7.chunk.js"
+  },
+  {
+    "revision": "fc2ed27a24acf0effdc1",
+    "url": "/cssa/static/js/runtime-main.2d2d57d6.js"
+  }
+]);
